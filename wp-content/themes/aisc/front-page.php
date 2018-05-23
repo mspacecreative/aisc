94d0019b8f99b50ec0945572b6ce6917642938d0
Matt <matt@mspacecreative.com>
2018-05-23 10:17:19 -0300

    c

--- wp-content/themes/aisc/front-page.php
+++ wp-content/themes/aisc/front-page.php
@@ -1,84 +1,76 @@
-<?php get_header(); ?>
+<?php
+
+get_header();
+
+$is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
+
+?>
 
 <div class="splash-graphic-overlay">
 	<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/driving-soc-eco-dev-img.png" alt="">
 </div>
 
 <div id="main-content">
+
+<?php if ( ! $is_page_builder_used ) : ?>
+
 	<div class="container">
 		<div id="content-area" class="clearfix">
 			<div id="left-area">
-		<?php
-			if ( have_posts() ) :
-				while ( have_posts() ) : the_post();
-					$post_format = et_pb_post_format(); ?>
 
-					<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
+<?php endif; ?>
 
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<?php if ( ! $is_page_builder_used ) : ?>
+
+					<h1 class="entry-title main_title"><?php the_title(); ?></h1>
 				<?php
 					$thumb = '';
 
 					$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
 
 					$height = (int) apply_filters( 'et_pb_index_blog_image_height', 675 );
-					$classtext = 'et_pb_post_main_image';
+					$classtext = 'et_featured_image';
 					$titletext = get_the_title();
 					$thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Blogimage' );
 					$thumb = $thumbnail["thumb"];
 
-					et_divi_post_format_content();
+					if ( 'on' === et_get_option( 'divi_page_thumbnails', 'false' ) && '' !== $thumb )
+						print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
+				?>
 
-					if ( ! in_array( $post_format, array( 'link', 'audio', 'quote' ) ) ) {
-						if ( 'video' === $post_format && false !== ( $first_video = et_get_first_video() ) ) :
-							printf(
-								'<div class="et_main_video_container">
-									%1$s
-								</div>',
-								$first_video
-							);
-						elseif ( ! in_array( $post_format, array( 'gallery' ) ) && 'on' === et_get_option( 'divi_thumbnails_index', 'on' ) && '' !== $thumb ) : ?>
-							<a class="entry-featured-image-url" href="<?php the_permalink(); ?>">
-								<?php print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height ); ?>
-							</a>
-					<?php
-						elseif ( 'gallery' === $post_format ) :
-							et_pb_gallery_images();
-						endif;
-					} ?>
-
-				<?php if ( ! in_array( $post_format, array( 'link', 'audio', 'quote' ) ) ) : ?>
-					<?php if ( ! in_array( $post_format, array( 'link', 'audio' ) ) ) : ?>
-						<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<?php endif; ?>
-
-					<?php
-						et_divi_post_meta();
-
-						if ( 'on' !== et_get_option( 'divi_blog_style', 'false' ) || ( is_search() && ( 'on' === get_post_meta( get_the_ID(), '_et_pb_use_builder', true ) ) ) ) {
-							truncate_post( 270 );
-						} else {
-							the_content();
-						}
-					?>
 				<?php endif; ?>
 
-					</article> <!-- .et_pb_post -->
-			<?php
-					endwhile;
+					<div class="entry-content">
+					<?php
+						the_content();
 
-					if ( function_exists( 'wp_pagenavi' ) )
-						wp_pagenavi();
-					else
-						get_template_part( 'includes/navigation', 'index' );
-				else :
-					get_template_part( 'includes/no-results', 'index' );
-				endif;
-			?>
+						if ( ! $is_page_builder_used )
+							wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
+					?>
+					</div> <!-- .entry-content -->
+
+				<?php
+					if ( ! $is_page_builder_used && comments_open() && 'on' === et_get_option( 'divi_show_pagescomments', 'false' ) ) comments_template( '', true );
+				?>
+
+				</article> <!-- .et_pb_post -->
+
+			<?php endwhile; ?>
+
+<?php if ( ! $is_page_builder_used ) : ?>
+
 			</div> <!-- #left-area -->
 
 			<?php get_sidebar(); ?>
 		</div> <!-- #content-area -->
 	</div> <!-- .container -->
+
+<?php endif; ?>
+
 </div> <!-- #main-content -->
 
 <?php
